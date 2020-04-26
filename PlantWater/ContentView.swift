daec77fd5881b52af592017a315c4cfedd41f52e
Marcus <marcus@tryfi.com>
2020-04-26 14:25:12 -0400

    Initial Commit

---
+++ PlantWater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlantWater
+//
+//  Created by Marcus Smith on 4/26/20.
+//  Copyright © 2020 marcusmth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
